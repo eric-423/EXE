b46e865da2a1f4b21cbf3a189cc2bcc34e55eb30
lequanghuyyyy <133076256+lequanghuyyyy@users.noreply.github.com>
2025-02-15 13:18:38 +0700

    add FE

---
+++ EXE_TamTac_FE/src/config/routes.ts
@@ -0,0 +1,21 @@
+const routes = {
+  public: {
+    home: "/",
+    login: "/login",
+    register: "/register",
+    verifyCode: "/verify-code",
+    forgotPassword: "/forgot-password",
+    setPassword: "/set-password",
+    aboutUs: "/about-us",
+    blog: "/blogs",
+    blogContent: "/blogs/:blogId",
+    stores: "/stores",
+    notFound: "/404",
+    hiring: "/hiring",
+  },
+  api: {
+    loginGoogle: "/api/auth/callback/google/redirect",
+  },
+};
+
+export default routes;
