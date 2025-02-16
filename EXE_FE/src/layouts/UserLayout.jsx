c0c5fc5d4afd0198ef6b36504bb63c4d12af8dce
alicee-19 <anh2k4.neko@gmail.com>
2025-02-16 12:30:35 +0700

    add router

---
+++ EXE_FE/src/layouts/UserLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const UserLayout = () => {
+  return (
+    <>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default UserLayout;
