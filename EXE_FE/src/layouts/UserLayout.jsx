a05cd138f8b1cab4d8886a8679002a7238f97f85
alicee-19 <anh2k4.neko@gmail.com>
2025-02-16 15:29:46 +0700

    add header

--- EXE_FE/src/layouts/UserLayout.jsx
+++ EXE_FE/src/layouts/UserLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
 
 const UserLayout = () => {
   return (
     <>
+      <Header />
       <main>
         <Outlet />
       </main>
