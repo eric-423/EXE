732416f92d22452fba96791ff40d174d2af998c5
lequanghuyyyy <133076256+lequanghuyyyy@users.noreply.github.com>
2025-02-18 13:41:59 +0700

    update homePage FE

--- EXE_FE/src/pages/home/HomePage.jsx
+++ EXE_FE/src/pages/home/HomePage.jsx
@@ -1,12 +1,16 @@
-import HomePageHeader from '../../components/homePage/HomePageHeader';
-import CuisineGallery from '../../components/homePage/CuisineGallery';
-import BestSeller from '../../components/homePage/BestSeller';
+import HomePageHeader from '../../components/homePage/homepage-header/HomePageHeader';
+import CuisineGallery from '../../components/homePage/cuisinegalley/CuisineGallery';
+import BestSeller from '../../components/homePage/best-seller/BestSeller';
+import Blogs from '../../components/homePage/blog/Blogs';
+import Franchise from '../../components/homePage/franchise/FranchiseComponent';
 
 const HomePage = () => {
   return <div>
     <HomePageHeader />
     <CuisineGallery />
     <BestSeller />
+    <Blogs />
+    <Franchise />
   </div>;
 };
 
