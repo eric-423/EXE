5ec510258f30980c7ed46f6edb4fd6b20e153d79
antretrou <156452783+antretrou@users.noreply.github.com>
2025-02-19 00:02:34 +0700

    update feedback, blacklist, dataseed

--- EXE_BE/models/orderitem.js
+++ EXE_BE/models/orderitem.js
@@ -3,31 +3,12 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-<<<<<<<< HEAD:EXE_BE/models/cookingutensil.js
-  class CookingUtensil extends Model {
-========
   class OrderItem extends Model {
->>>>>>>> An:EXE_BE/models/orderitem.js
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-<<<<<<<< HEAD:EXE_BE/models/cookingutensil.js
-    static associate({ UtensilsType }) {
-      // define association here
-      this.belongsTo(UtensilsType, { foreignKey: "type" })
-    }
-  }
-  CookingUtensil.init({
-    name: DataTypes.STRING,
-    quantity: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'CookingUtensil',
-  });
-  return CookingUtensil;
-========
     static associate({ Order, Product }) {
       // define association here
       this.belongsTo(Product, { foreignKey: "productId" })
@@ -37,11 +18,14 @@ module.exports = (sequelize, DataTypes) => {
   OrderItem.init({
     quantity: DataTypes.INTEGER,
     price: DataTypes.FLOAT,
-    note: DataTypes.STRING
+    note: DataTypes.STRING,
+    feedback: DataTypes.STRING,
+    isFeedback: DataTypes.BOOLEAN,
+    feedbackPoint: DataTypes.INTEGER,
+    experiedFeedbackTime: DataTypes.DATE
   }, {
     sequelize,
     modelName: 'OrderItem',
   });
   return OrderItem;
->>>>>>>> An:EXE_BE/models/orderitem.js
 };
