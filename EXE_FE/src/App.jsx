dd550f263da1cea6b0d284eb1f6527b21eb75e5f
trungtruongit <hieutrungtruong@gmail.com>
2025-02-19 00:26:15 +0700

    solve conflict change in json and lock.josn

--- EXE_FE/src/App.jsx
+++ EXE_FE/src/App.jsx
@@ -1,10 +1,6 @@
 import "./index.css"; // Ensure the CSS file is imported here as well
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
 
-=======
-// import "font-awesome/css/font-awesome.min.css";
->>>>>>> 4941902b8994b0112033a88c488d2031d8c2b663
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesComponent from "./routes";
