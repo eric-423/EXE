95f0cb3e0b3cba473a63f46139c56858597a4867
lequanghuyyyy <133076256+lequanghuyyyy@users.noreply.github.com>
2025-02-18 15:25:21 +0700

    done home page

---
+++ EXE_FE/src/components/homePage/header/Header.jsx
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+import { Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Row className="header">
+            <Col className='col-md-9 header-left'>
+                <img src="/images/logo.png" alt="" />
+                <Link className='redirect' to="/">Về tấm tắc</Link>
+                <Link className='redirect' to="/">Chuyện cơm tấm</Link>
+                <Link className='redirect' to="/">Đặt hàng</Link>
+                <Link className='redirect' to="/">Nhượng quyền</Link>
+                <Link className='redirect' to="/">Cửa hàng</Link>
+                <Link className='redirect' to="/">Tuyển dụng</Link>
+            </Col>
+            <Col>
+                <div className="header-right">
+                    <img src="/icons/notification-3.svg" alt="" />
+                    <img src="/icons/account.svg" alt="" />
+                    <img src="/icons/shopping-cart-outline-badged.svg" alt="" />
+                </div>
+            </Col>
+
+        </Row>
+    )
+}
+
+export default Header
--- EXE_FE/src/pages/home/HomePage.jsx
+++ EXE_FE/src/pages/home/HomePage.jsx
@@ -3,14 +3,18 @@ import CuisineGallery from '../../components/homePage/cuisinegalley/CuisineGalle
 import BestSeller from '../../components/homePage/best-seller/BestSeller';
 import Blogs from '../../components/homePage/blog/Blogs';
 import Franchise from '../../components/homePage/franchise/FranchiseComponent';
+import Header from '../../components/homePage/Header/Header';
+import Footer from '../../components/homePage/Footer/Footer';
 
 const HomePage = () => {
   return <div>
+    <Header></Header>
     <HomePageHeader />
     <CuisineGallery />
     <BestSeller />
     <Blogs />
     <Franchise />
+    <Footer />
   </div>;
 };
 
