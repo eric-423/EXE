0c4de9462e750d7f5006031580eabdafc4ce5629
lequanghuyyyy <133076256+lequanghuyyyy@users.noreply.github.com>
2025-02-26 13:55:49 +0700

    ahahaaha

--- EXE_FE/src/components/checkoutPage/checkoutRight.jsx
+++ EXE_FE/src/components/checkoutPage/checkoutRight.jsx
@@ -1,19 +1,36 @@
-import { Col } from 'react-bootstrap'
-import { useState } from 'react';
+import { Card, CardText, CardTitle, Col, Form } from 'react-bootstrap'
+import { useEffect, useState } from 'react';
 
 const CheckoutRight = () => {
     const [promoCode, setPromoCode] = useState('');
     const [note, setNote] = useState('');
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [discountValue, setDiscountValue] = useState(0);
+    const [discountOnItem, setDiscountOnItem] = useState(0);
+    const [shippingFee, setShippingFee] = useState(0);
 
     const handleApplyPromo = () => {
         console.log('Mã khuyến mãi:', promoCode);
+        setDiscountValue(0)
     };
+
+    const setTotalAmount = () => {
+        let price = 0;
+        combo.forEach(item => {
+            price += parseInt(item.price) * parseInt(item.quantity);
+        });
+        setTotalPrice(price);
+    };
+
+    useEffect(() => {
+        setTotalAmount();
+    })
 
     const combo = [
         {
             name: "Cơm tấm sườn bì chả",
             price: "35.000đ",
-            quantity: "x1",
+            quantity: "1",
             additionalItems: [
                 { name: "Chả" },
                 { name: "Trứng" }
@@ -22,7 +39,7 @@ const CheckoutRight = () => {
         {
             name: "Cơm tấm sườn bì chả",
             price: "35.000đ",
-            quantity: "x1",
+            quantity: "1",
             additionalItems: [
                 { name: "Chả" },
                 { name: "Trứng" }
@@ -31,13 +48,13 @@ const CheckoutRight = () => {
         {
             name: "Canh chua cá",
             price: "20.000đ",
-            quantity: "x1",
+            quantity: "1",
         }
     ]
 
     return (
         <Col md={5} >
-            <div className='comfirm-order'>
+            <div className='comfirm-order rounded-4'>
                 <div className='mb-4 '
                     style={{ backgroundColor: 'transparent', borderBottom: '1px solid black' }}
                 >
@@ -53,7 +70,7 @@ const CheckoutRight = () => {
 
                                 <div className="d-flex justify-content-end gap-3">
                                     <p style={{ color: 'rgb(218, 115, 50)' }}>{item.price}</p>
-                                    <p>{item.quantity}</p>
+                                    <p> x {item.quantity}</p>
                                 </div>
 
                             </div>
@@ -70,7 +87,7 @@ const CheckoutRight = () => {
                 }
             </div>
 
-            <div className='promotion-code p-4'>
+            <div className='promotion-code p-4 rounded-4'>
                 <div style={{ marginBottom: '8px', display: 'flex' }}>
                     <input
                         type="text"
@@ -93,6 +110,45 @@ const CheckoutRight = () => {
                     onChange={(e) => setNote(e.target.value)}
                 />
             </div>
+
+
+            <Form className=' mt-5 rounded-4 card-total-price p-3'>
+                <div>
+                    <div className='d-flex justify-content-between pl-3 pr-3'>
+                        <h5>Giá gốc</h5>
+                        <p>{totalPrice}.000 đ</p>
+                    </div>
+                </div>
+                <div >
+                    <div className='d-flex justify-content-between pl-3 pr-3'>
+                        <h5>Giảm giá trên món</h5>
+                        <p>{discountOnItem} đ</p>
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex justify-content-between pl-3 pr-3'>
+                        <h5>Giảm giá khuyến mãi</h5>
+                        <p>{discountValue} đ</p>
+                    </div>
+                </div>
+                <div style={{ borderBottom: '1px solid black' }} className='mb-3'>
+                    <div className='d-flex justify-content-between pl-3 pr-3'>
+                        <h5>Phí giao hàng</h5>
+                        <p>{shippingFee} đ</p>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='d-flex justify-content-between pl-3 pr-3'>
+                        <h5>Tổng cộng</h5>
+                        <p>{totalPrice - discountValue + shippingFee - discountOnItem} đ</p>
+                    </div>
+                </div>
+
+                <button className='w-100 mt-3 rounded-3'>
+                    Đặt hàng
+                </button>
+            </Form>
 
         </Col>
     )
