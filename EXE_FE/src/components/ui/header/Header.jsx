ca23f9a957f3480f33e1b66d0141b3e66feedce5
Trương Quang Hiếu Trung <hieutrungtruong@gmail.com>
2025-02-27 21:13:25 +0700

    change header

--- EXE_FE/src/components/ui/header/Header.jsx
+++ EXE_FE/src/components/ui/header/Header.jsx
@@ -1,30 +1,63 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Header.css';
-import { Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Header.css";
+import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <Row className="header">
-            <Col className='col-md-9 header-left'>
-                <img src="/images/logo.png" alt="" />
-                <Link className='redirect' to="/">Về tấm tắc</Link>
-                <Link className='redirect' to="/">Chuyện cơm tấm</Link>
-                <Link className='redirect' to="/">Đặt hàng</Link>
-                <Link className='redirect' to="/">Nhượng quyền</Link>
-                <Link className='redirect' to="/">Cửa hàng</Link>
-                <Link className='redirect' to="/">Tuyển dụng</Link>
-            </Col>
-            <Col>
-                <div className="header-right">
-                    <img src="/icons/notification-3.svg" alt="" />
-                    <img src="/icons/account.svg" alt="" />
-                    <img src="/icons/shopping-cart-outline-badged.svg" alt="" />
+            <Col className="col-lg-9 col-md-8 col-sm-12 header-left">
+                <Link to="/">
+                    <img src="/images/logo.png" alt="logo" className="logo" />
+                </Link>
+                <div className="nav-links">
+                    <Link className="redirect" to="/">
+                        Về tấm tắc
+                    </Link>
+                    <Link className="redirect" to="/">
+                        Chuyện cơm tấm
+                    </Link>
+                    <Link className="redirect" to="/">
+                        Đặt hàng
+                    </Link>
+                    <Link className="redirect" to="/">
+                        Nhượng quyền
+                    </Link>
+                    <Link className="redirect" to="/">
+                        Cửa hàng
+                    </Link>
+                    <Link className="redirect" to="/">
+                        Tuyển dụng
+                    </Link>
                 </div>
             </Col>
-
+            <Col className="col-lg-3 col-md-4 col-sm-12">
+                <div className="header-right">
+                    <Link to="/login" className="icon-link">
+                        <img
+                            src="/icons/notification-3.svg"
+                            alt="notifications"
+                            className="header-icon"
+                        />
+                    </Link>
+                    <Link to="/login" className="icon-link">
+                        <img
+                            src="/icons/account.svg"
+                            alt="account"
+                            className="header-icon"
+                        />
+                    </Link>
+                    <Link to="/login" className="icon-link">
+                        <img
+                            src="/icons/shopping-cart-outline-badged.svg"
+                            alt="cart"
+                            className="header-icon"
+                        />
+                    </Link>
+                </div>
+            </Col>
         </Row>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
