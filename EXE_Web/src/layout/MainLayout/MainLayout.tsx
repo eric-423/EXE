7f3bb1ef2a58a5564cc23bf370f7e7240f9c2710
alicee-19 <anh2k4.neko@gmail.com>
2025-06-05 01:06:46 +0700

    fix: remove chatbot button

--- EXE_Web/src/layout/MainLayout/MainLayout.tsx
+++ EXE_Web/src/layout/MainLayout/MainLayout.tsx
@@ -16,20 +16,6 @@ const MainLayout = () => {
         <Outlet />
       </main>
       <Footer />
-      <div className='fixed bottom-12 right-12 z-50'>
-        <Button
-          size='lg'
-          className='!p-0 bg-foreground text-gray-50 hover:bg-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 shadow-lg transition-colors duration-300 w-16 h-16 gap-0 rounded-full'
-          onClick={handleChatbotClick}
-        >
-          <BotMessageSquare className='h-29 w-29' />
-          <span className='sr-only'>Open</span>
-        </Button>
-        {open && (
-          //Embed the script to load the chatbot
-          <></>
-        )}
-      </div>
     </>
   );
 };
